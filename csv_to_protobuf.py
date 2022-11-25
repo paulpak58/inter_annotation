e6a26306077816fc8237247e61c8d0dbe36eda3f
paulpak58 <paulpak58@gmail.com>
2022-11-25 09:48:30 -0600

    CSV to annotation edits

--- csv_to_protobuf.py
+++ csv_to_protobuf.py
@@ -3,15 +3,13 @@ import csv
 import glob
 import os
 import uuid
-
 import google
 import numpy as np
 import datetime
-
-from data_interface import sages_pb2
+import sages_pb2
+from sages_pb2 import *
+import multidict
 import google.protobuf.timestamp_pb2
-
-# python ./action_triplet_convert.py --dataset-folder DATADIR OUTDIR
 
 def parse_args():
     """Parse arguments
@@ -72,8 +70,6 @@ def create_track_groups(track_type, folder, class_names, label_type):
     labels_list = list()
     invalid_symbols = ['N/A', 'NA', '']
     for filename in txt_files:
-        print(filename)
-        
         if params["verbose"]:
             print("annotation filename: {}".format(filename))
         with open(filename, "r") as fp:
@@ -93,7 +89,7 @@ def create_track_groups(track_type, folder, class_names, label_type):
                     for idx_column in range(len(row)-1):
                         row_name_tmp = row[idx_column].split('. ')[-1]
                         row_name.append(row_name_tmp)
-                
+
                 else:
 
                     for i_column in range(1, len(row)-1):
@@ -104,8 +100,8 @@ def create_track_groups(track_type, folder, class_names, label_type):
                         if video_filename == '':
                             continue
 
-                        if (row[i_column].replace(' ', '') not in invalid_symbols and row[i_column+1].replace(' ', '') not in invalid_symbols) and 'start' in row_name[i_column]:
-                            row_label = row_name[i_column].split(' start')[0].split(' end')[0] 
+                        if (row[i_column].replace(' ', '') not in invalid_symbols and row[i_column+1].replace(' ', '') not in invalid_symbols) and 'Start' in row_name[i_column]:
+                            row_label = row_name[i_column].split(' Start')[0].split(' End')[0] 
                             
                             start_time = (datetime.datetime.strptime(row[i_column].replace(' ', ''), "%H:%M:%S") - datetime.datetime(1900, 1, 1)).total_seconds()
                             end_time = (datetime.datetime.strptime(row[i_column+1].replace(' ', ''), "%H:%M:%S") - datetime.datetime(1900, 1, 1)).total_seconds()
@@ -140,23 +136,22 @@ def create_track_groups(track_type, folder, class_names, label_type):
                                 start=protobuf_start_time, end=protobuf_end_time, start_exact=True, end_exact=True
                             )
 
-
                             new_event = sages_pb2.Event(
-                                type=label, temporal_interval=protobuf_interval, video_id=video_id, annotator_id='jeckhoff'
+                                type=label, temporal_interval=protobuf_interval, video_id=video_id, annotator_id=row[0]+'_'+row[1]
                             )
                             new_entity = sages_pb2.SurgeryEntity(event=new_event, entity_id=new_id)
 
                             surgical_entities.append(new_entity)
                             interval_start_id = row_id
-                        elif row[i_column].replace(' ', '') not in invalid_symbols and row[i_column+1].replace(' ', '') in invalid_symbols and 'start' in row_name[i_column]:
+                        elif row[i_column].replace(' ', '') not in invalid_symbols and row[i_column+1].replace(' ', '') in invalid_symbols and 'Start' in row_name[i_column]:
                             print(video_filename)
-                            print(row_name[i_column].split(' start')[0].split(' end')[0] + "     start:" + row[i_column] + "    end:" +   row[i_column+1])
+                            print(row_name[i_column].split(' Start')[0].split(' End')[0] + "     Start:" + row[i_column] + "    End:" +   row[i_column+1])
 
                     #sort according to starting time before adding to track
                     surgical_entities.sort(key=lambda x:x.event.temporal_interval.start.ToSeconds())
                     track = sages_pb2.Track(name=track_type, entities=surgical_entities)
                     tg = sages_pb2.TracksGroup(name=track_type + "_group", tracks=[track])
-
+                    print(video_filename)
                     results_dict[video_filename] = tg
 
     print(str(labels_set))
@@ -173,8 +168,8 @@ def load_csv(filename=None):
         for i_row, row in enumerate(csv_data):
             if i_row == 0:
                 for i_column, column in enumerate(row[3:]):
-                    if 'start' in column or 'end' in column:
-                        anno_name = column.split('.')[-1].split(' start')[0].split(' end')[0].replace(' ', '')
+                    if 'Start' in column or 'End' in column:
+                        anno_name = column.split('.')[-1].split(' Start')[0].split(' End')[0].replace(' ', '')
                         anno_label = 1.0
                         anno[anno_name] = anno_label
     return anno
@@ -201,6 +196,111 @@ def load_class_names(dataset_folder = None):
     return class_names
 
 
+def load_protobuf_dir(
+    annotation_dir,
+    missing_annotations_is_okay=False,
+    prefix="",
+    verbose=True,
+    phase_translation_file=None,
+    allowed_track_names=None,
+):
+    """
+    load annotation information from protobuf dataset
+    :param annotation_dir: a folder contains .pb files
+    :param missing_annotations_is_okay:
+    :param prefix: prefix string if any
+    :param verbose:
+    :param phase_translation_file: the phase translation filename
+    :param allowed_track_names: the track names that will actually be read. Use None to allow all tracks.
+    :return:
+    """
+    steps = {}
+    annotations = {}
+
+    files_list = sorted(glob.glob(os.path.join(annotation_dir, "*.pb")))
+    if phase_translation_file is not None:
+        from data_interface.base_utils import read_phase_mapping
+
+        phase_name_map = read_phase_mapping(phase_translation_file)
+    else:
+        phase_name_map = {}
+
+    for filename in files_list:
+        try:
+            tracks = load_protobuf_file(filename)
+        except:
+            print("Error in " + filename)
+            raise
+
+        video_file = os.path.split(filename)[-1].split(".")[0]
+        annotations[os.path.join(prefix, video_file)] = multidict.MultiDict()
+        total_time = 0
+        pre_phase_name = ""
+        for trk in tracks:
+            if trk["track_name"] == "point" or trk["track_name"] == "other steps":
+                continue
+            track_name = trk["track_name"]
+            if allowed_track_names is not None and track_name not in allowed_track_names:
+                continue
+
+            props = trk["entity"].event
+            name = props.type
+            if name in phase_name_map.keys():
+                name = phase_name_map[name]
+
+            start = float(str(props.temporal_interval.start.seconds) + "." + str(props.temporal_interval.start.nanos))
+            end = float(str(props.temporal_interval.end.seconds) + "." + str(props.temporal_interval.end.nanos))
+            temporal_attr = "interval"
+            if start == 0 and end == 0:
+                temporal_attr = "point"
+                start = float(str(props.temporal_point.point.seconds) + "." + str(props.temporal_point.point.nanos))
+                end = start
+            video_id = props.video_id
+            annotator_id = props.annotator_id
+            if name is None:
+                if verbose:
+                    print("Attribute name is missing, " + filename)
+                continue
+            if track_name not in steps:
+                steps[track_name] = set()
+            steps[track_name].add(name)
+            annotations[os.path.join(prefix, video_file)].add(
+                name,
+                {
+                    "start": start,
+                    "end": end,
+                    "temporal_attr": temporal_attr,
+                    "annotator_id": annotator_id,
+                    "track_name": track_name,
+                    "video_id": video_id,
+                },
+            )
+            total_time += float(end) - float(start)
+            if track_name == "major operative phases":
+                pre_phase_name = name
+    if len(annotations) == 0 and not missing_annotations_is_okay:
+        print("Missing annotations: " + annotation_dir)
+        raise MissingAnnotationsError(annotation_dir)
+    # sort the class_name in alphabet order, otherwise each training has different class names
+    class_names = {}
+    for key in steps:
+        class_names[key] = list(steps[key])
+        class_names[key].sort()
+    return class_names, annotations
+
+
+def load_protobuf_file(filename):
+    with open(filename, "rb") as fp:
+        annotation_set = AnnotationSet()
+        annotation_set.ParseFromString(fp.read())
+        all_entities = []
+        for tracks_group in annotation_set.tracks_groups:
+            for tr in tracks_group.tracks:
+                for entity in tr.entities:
+                    all_entities.append({"track_name": tr.name, "entity": entity})
+    return all_entities
+
+
 
 if __name__ == "__main__":
     args = parse_args()
@@ -208,25 +308,39 @@ if __name__ == "__main__":
     annotation_set_dicts = {}
     dataset_folder = params["dataset_folder"]
     class_names = load_class_names(dataset_folder)
+    print(class_names.keys())
     for track_name in class_names.keys():
+    # for track_name in class_names['phase']:
         track_dicts = create_track_groups(track_type=track_name,
                                           folder=dataset_folder,
-                                          class_names = class_names[track_name],
+                                          # class_names = class_names['phase'],
+                                          class_names = class_names,
                                           label_type='label_name')
-
         for key in track_dicts:
             if key not in annotation_set_dicts:
                 annotation_set_dicts[key] = []
             annotation_set_dicts[key].append(track_dicts[key])
+    print(annotation_set_dicts)
+ 
 
+    if False:
+        for key in annotation_set_dicts:
+            annotation_set = sages_pb2.AnnotationSet(tracks_groups=annotation_set_dicts[key])
+            folder_name = os.path.expandvars(os.path.expanduser(params['output']))
+            os.makedirs(folder_name, exist_ok=True)
+            
+            annotator_name = 'test'
+            output_pathname = os.path.join(folder_name,key + '_{}.pb'.format(annotator_name))
+            with open(output_pathname,'wb') as fp:
+                fp.write(annotation_set.SerializeToString())
+                fp.close()
 
-    for key in annotation_set_dicts:
-        annotation_set = sages_pb2.AnnotationSet(tracks_groups=annotation_set_dicts[key])
-        folder_name = os.path.expandvars(os.path.expanduser(params['output']))
-        os.makedirs(folder_name, exist_ok=True)
-
-        output_pathname = os.path.join(folder_name,key + '.pb')
-        with open(output_pathname,'wb') as fp:
-            fp.write(annotation_set.SerializeToString())
-            fp.close()
-    
+    if False:
+        # Reading from pb files
+        # print(load_protobuf_file('outputs/LC39.pb'))
+        class_names,annotations = load_protobuf_dir('outputs')
+        print('Class Names',class_names)
+        print('Annotation',annotations.keys())
+        print('\n',annotations['LC39'],'\n')
+        print('\n',annotations['LC147'],'\n')
+        print('\n',annotations['LC185'],'\n')
---
+++ outputs/LC147.pb
@@ -0,0 +1,24 @@
+
+
+phase_group‡
+phase[
+3Port Placement 1
+çè :LC147Bjeckhoff"$8b4af12c-d77a-4072-81df-46d9f46b31ee\
+4Fundus Retraction
+Ä :LC147Bjeckhoff"$5c4e2bde-79d9-4555-9c54-3f3758d52bban
+F#Release of Gallbladder Peritoneum 1
+Ä“ :LC147Bjeckhoff"$696db2b6-e0d7-4784-9c21-bf319b9714d2W
+/Checkpoint 1
+˚ˇ :LC147Bjeckhoff"$0f83dc76-dc13-41fc-892a-53b652584217_
+7Clipping Cystic Duct
+ˇ± :LC147Bjeckhoff"$f7df9d1e-5a09-4574-bef0-db8fe34059aba
+9Clipping Cystic Artery
+ˇ± :LC147Bjeckhoff"$1d03bca1-75d9-4394-8481-dd8c19f3bde2a
+9Division Cystic Artery
+≤∑ :LC147Bjeckhoff"$940b96cc-a6de-4d72-8488-b4dca8c104ea_
+7Division Cystic Duct
+≤∑ :LC147Bjeckhoff"$1fb8676f-63c8-4073-adce-bf9212cdebccW
+/Checkpoint 2
+∑∆ :LC147Bjeckhoff"$e45376ac-dc01-4afe-81ea-aac340789175r
+J'Removal of Gallbladder from Liver Bed 1
+∆∆ :LC147Bjeckhoff"$9a684025-f14a-40a7-9b35-6c4c8d14e287
