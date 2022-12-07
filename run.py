cfb51aa0598b3b2a859d9aa629a32a44b17a88d6
paulpak58 <paulpak58@gmail.com>
2022-12-07 15:46:17 -0600

    run bug fixes

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 import csv
+import numpy as np
 import argparse
 from itertools import combinations
-from inter_annotate import cohen_kappa_score,cohen_kappa
+from inter_annotate import cohen_kappa_score,cohen_kappa,fleiss_kappa
 from retrieve_annotations import load_class_names,create_track_groups,write_files,load_protobuf_dir,retrieve_annotator_classifications
 
 def parse_args(): 
@@ -40,10 +41,10 @@ if __name__ == "__main__":
     write_files(annotation_set_dicts,params)
 
     # Load and read protobuf files
-    class_names,annotations = load_protobuf_dir('output_protobufs')
+    class_names,annotations = load_protobuf_dir(params["output"])
 
     # Retrieve the annotations
-    unique_keys,true_pairs,true_phases,IAA = retrieve_annotator_classifications(class_names,annotations)
+    unique_keys,true_pairs,true_phases,IAA,FLEISS_IAA = retrieve_annotator_classifications(class_names,annotations)
 
     # Write inter-agreement annotations to output file
     output_dir_cohen = 'results/IAA_cohen' if params["results_cohen"] is None else str(params["results_cohen"])
@@ -54,7 +55,7 @@ if __name__ == "__main__":
     header = ['Video name','User Pair','']
     for phase in true_phases:
         header.append(phase)
-    header.append('Total Cohen IAA')
+    # header.append('Total Cohen IAA')
     
     rows = list()
     for video in unique_keys:
@@ -84,14 +85,41 @@ if __name__ == "__main__":
                     total_first_annotation_video += first_annotation_arr
                     total_second_annotation_video += second_annotation_arr
             total_score = cohen_kappa(total_first_annotation_video,total_second_annotation_video)
-            row['Total Cohen IAA'] = total_score
+            # row['Total Cohen IAA'] = total_score
             rows.append(row)
-    # print(rows)
+
+    '''
+    # Compute Total Averages
+    avg_row = list()
+    for annotator_pair in true_pairs:
+        row['Video name'] = 'Average'
+        first_annotator = annotator_pair[0]
+        second_annotator = annotator_pair[1]
+        row['User Pair'] = first_annotator+','+second_annotator
+        row[''] = ''
+        for video in unique_keys():
+            for phase in true_phases:
+    '''
+
     with open(output_fn_cohen,'w') as f:
         writer = csv.DictWriter(f,fieldnames=header)
         writer.writeheader()
         writer.writerows(rows)
+
     '''
+    header = ['Video name','User Group','']
+    for phase in true_phases:
+        header.append(phase)
+    header.append('Total Cohen IAA')
+    
+    rows = list()
+    for video in unique_keys:
+        M = FLEISS_IAA[video]
+        M = np.array(M)
+        print(M[:,1:].shape)
+        score = fleiss_kappa(M[:,1:])
+        score2 = fleiss_kappa(M)
+        print('Score 1: ',score,' Score 2: ',score2)
     with open(output_fn_fleiss,'w') as f:
         writer = csv.DictWriter(f,fieldnames=header)
         writer.writeheader()
