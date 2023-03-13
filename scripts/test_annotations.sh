d064ee6f328806c85b0b7f081b708823d49b5ef9
paulpak58 <paulpak58@gmail.com>
2023-03-13 09:25:20 -0500

    Update env specs

--- scripts/test_annotations.sh
+++ scripts/test_annotations.sh
@@ -5,8 +5,7 @@ outputs=../output_protobufs
 # Change to source file
 source=../run.py
 # Change to output file
-results_cohen=../results/IAA_cohen
-# Not Implemented (here for future ref)
+results_cohen=../results/IAA_cohen_test
 results_fleiss=../results/IAA_fleiss
 
 python ${source} --dataset-folder ${data} --output ${outputs} --results_cohen ${results_cohen} --results_fleiss ${results_fleiss}
