c612c4dece70fd8617c9c43a365f3382d2bb5020
paulpak58 <paulpak58@gmail.com>
2023-06-12 21:45:44 -0700

    updated qualification type

--- AMT/qualification_type.py
+++ AMT/qualification_type.py
@@ -18,8 +18,9 @@ test_question = '''
     <DisplayName>I have reviewed  the Annotation Protocol.</DisplayName>
     <IsRequired>true</IsRequired>
     <QuestionContent>
-    <Text>Please confirm that you have read the contents of the Annotation Protocol by selecting "Yes".</Text>
-    <FormattedContent><![CDATA[<a href="https://mghvideos.s3.us-east-2.amazonaws.com/Annotation+Protocol+AmTurk.pdf">Annotation Protocol PDF</a>]]></FormattedContent>
+    <Text>Please confirm that you have read the contents of the Annotation Protocol and the IRB Agreement by selecting "Yes".</Text>
+    <FormattedContent><![CDATA[<a href="https://mghvideos.s3.us-east-2.amazonaws.com/Annotation+Protocol+AmTurk.pdf">Annotation Protocol Guidelines</a>]]></FormattedContent>
+    <FormattedContent><![CDATA[<a href="https://mghvideos.s3.us-east-2.amazonaws.com/VerbalConsent_AMTurk.pdf">IRB Agreement</a>]]></FormattedContent>
     </QuestionContent>
     <AnswerSpecification>
     <SelectionAnswer>
@@ -28,6 +29,10 @@ test_question = '''
         <Selection>
             <SelectionIdentifier>Yes</SelectionIdentifier>
             <Text>Yes</Text>
+        </Selection>
+        <Selection>
+            <SelectionIdentifier>No</SelectionIdentifier>
+            <Text>No</Text>
         </Selection>
         </Selections>
     </SelectionAnswer>
@@ -40,26 +45,49 @@ test_question = '''
 answer_text = '''
 <AnswerKey xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/AnswerKey.xsd">
     <Question>
-    <QuestionIdentifier>read_google_drive_folder</QuestionIdentifier>
-    <AnswerOption>
-        <SelectionIdentifier>yes</SelectionIdentifier>
-        <AnswerScore>1</AnswerScore>
-    </AnswerOption>
+        <QuestionIdentifier>ReadAnnotationProtocol</QuestionIdentifier>
+        <AnswerOption>
+            <SelectionIdentifier>Yes</SelectionIdentifier>
+            <AnswerScore>1</AnswerScore>
+        </AnswerOption>
+        <AnswerOption>
+            <SelectionIdentifier>No</SelectionIdentifier>
+            <AnswerScore>0</AnswerScore>
+        </AnswerOption>
     </Question>
 </AnswerKey>
 '''
 
 
-# Qualification Type
-response = client.create_qualification_type(
-    Name='AnnotationProtocol',
-    Description='Please read and remember the definitions of start and endpoint of surgical phases in laparoscopic cholecystectomy.',
-    QualificationTypeStatus='Active',
-    Keywords='video, classification, laparoscopic cholecystectomy, annotation, protocol',
-    AutoGranted=False,
-    RetryDelayInSeconds=86400,          # 24 hours
-    TestDurationInSeconds=3600,             # 1 hour
-    Test=test_question,
-    AnswerKey=answer_text
-)
-print(response['QualificationType']['QualificationTypeId'])
+qualification_type_name = 'AnnotationProtocol'
+response = client.list_qualification_types(Query=qualification_type_name, MustBeRequestable=False)
+if response['NumResults'] > 0:
+    qualification_type_id = response['QualificationTypes'][0]['QualificationTypeId']
+    response = client.update_qualification_type(
+        # Name='AnnotationProtocol',
+        QualificationTypeId=qualification_type_id,
+        Description='Please read and remember the definitions of start and endpoint of surgical phases in laparoscopic cholecystectomy and read the verbal IRB agreement form.',
+        QualificationTypeStatus='Active',
+        AutoGranted=False,
+        # Keywords='video, classification, laparoscopic cholecystectomy, annotation, protocol',
+        RetryDelayInSeconds=0, \
+        # RetryDelayInSeconds=43200,              # 12 hours
+        TestDurationInSeconds=3600,             # 1 hour
+        Test=test_question,
+        AnswerKey=answer_text
+    )
+else:
+    # Qualification Type
+    response = client.create_qualification_type(
+        Name='AnnotationProtocol',
+        Description='Please read and remember the definitions of start and endpoint of surgical phases in laparoscopic cholecystectomy and read the verbal IRB agreement form.',
+        QualificationTypeStatus='Active',
+        Keywords='video, classification, laparoscopic cholecystectomy, annotation, protocol',
+        AutoGranted=False,
+        RetryDelayInSeconds=0, \
+        # RetryDelayInSeconds=43200,              # 12 hours
+        TestDurationInSeconds=3600,             # 1 hour
+        Test=test_question,
+        AnswerKey=answer_text
+    )
+    print(response['QualificationType']['QualificationTypeId'])
